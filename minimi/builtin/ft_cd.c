1cae7e00b462f30d69b7298bfffc4ccf170bcf39
Julien Guerin <jguerin@k2r1p5.42mulhouse.fr>
2024-02-15 10:42:40 +0100

    ajout fonction cd WIP

--- minimi/builtin/ft_cd.c
+++ minimi/builtin/ft_cd.c
@@ -6,29 +6,43 @@
 /*   By: jguerin <jguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 09:38:07 by jguerin           #+#    #+#             */
-/*   Updated: 2024/02/14 12:12:03 by jguerin          ###   ########.fr       */
+/*   Updated: 2024/02/15 10:14:22 by jguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+
 int	ft_cd(char **argv, t_shell *shell)
 {
 	int i = 1;
 	int j = 0;
+	int error;
 	char *path;
 
-	if(shell->argc == 0)
-		printf("ouah chaud");
+	if(shell->argc == 1)
+		return (1);
 	else if (shell->argc > 2)
 	{
 		ft_putstr_fd(argv[i], 2);
 		ft_putstr_fd(": Too many arguments\n", 2);
 		return (1);
 	}
-	else if (argv[i][j] == '~')
+	if (argv[i][j] == '~')
 	{
 		path = ft_getenv("HOME", shell->env);
-		chdir(path);
+		if (chdir(path) != 0)
+			ft_putstr_fd("No such file or directory v2\n", 2);
+	}
+	else if (argv[i])
+	{
+		path = argv[i];
+		error = chdir(path);
+		if (error)
+		{
+			ft_putstr_fd(argv[i], 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+			return(1);
+		}
 	}
 }
